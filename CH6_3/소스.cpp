bba1f9c7bfc7e1d48ca464112548744c1f785014
penguinman-99 <lateson6@gmail.com>
2020-12-31 20:53:08 +0900

    add Remark

--- CH6_3/소스.cpp
+++ CH6_3/소스.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
+using namespace std;
+
+int main() {
+	const int num_stu = 5;
+	int score[num_stu] = { 100,50,92,76,54 };
+	int max_score = 0;
+	int result = 0;
+	//평균과 최고점을 기록하는 소스
+	for (int i = 0; i < num_stu; i++) {
+		result += score[i];
+		max_score = (max_score < score[i] ? max_score = score[i] : max_score);
+	}
+	cout << result << " " << result / 5 << " " << max_score;
 
 
-int main()
-{
 
-    return 0;
+
+
 }
