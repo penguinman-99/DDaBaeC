e270a9de604d7aa95de6fc09dc2705e64eb4affc
penguinman-99 <lateson6@gmail.com>
2021-01-01 18:00:57 +0900

    make selection sort code

--- CH_6/CH6_4/소스.cpp
+++ CH_6/CH6_4/소스.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 using namespace std;
+void printArr(const int arr[],const int length) {
 
+	for (int i = 0; i < length; i++) {
+		cout << arr[i];
+
+	}
+	cout << endl;
+}
 int main() {
+	/*
+	3 5 2 1 4
+	1 5 2 3 4
+	1 2 5 3 4
+	1 2 3 5 4
+	1 2 3 4 5
+	*/
+	const int length = 5;
+	int arr[length] = { 3,5,2,1,4 };
+	for (int i = 0; i < length-1; i++) {
+		int init_idx = i;
+		for (int j = i+1; j < length; j++) {
+			if (arr[i] > arr[j]) {
+				int tp1;
+				tp1 = arr[j];
+				arr[j] = arr[i];
+				arr[i] = tp1;
 
+			}
 
+		}
 
+		printArr(arr, length);
+
+	}
+
+	return 0;
 
 }
